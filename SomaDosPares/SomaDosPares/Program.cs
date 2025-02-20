28b6cebd1391149de60a1ad2371469d7f803204e
FelipeS3 <Felipexy50@gmail.com>
2025-02-20 06:28:11 -0300

    soma-pares

---
+++ SomaDosPares/SomaDosPares/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace SomaDosPares
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            SomaPares();
+        }
+        static void SomaPares()
+        {
+            Console.WriteLine("Digite um numero");
+            int num = Convert.ToInt32(Console.ReadLine());
+            int soma = 0;
+
+            for (int i = 0; i <= num; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    soma += i;
+                }
+            }
+            Console.WriteLine($"A soma dos numeros pares de 1 ate {num} é {soma}");
+
+        }
+    }
+}
