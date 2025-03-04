c4760e9cf7b78503c767d94e52fc9f64471b982b
FelipeS3 <Felipexy50@gmail.com>
2025-03-04 10:18:05 -0300

    Exercicio-TryCatch

---
+++ exercicio-try-catch-finally/exercicio-try-catch-finally/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace exercicio_try_catch_finally
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Digite um numero");
+                int num1 = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("Digite o segundo numero");
+                int num2 = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine(num1 / num2);
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Erro: Você deve digitar um número válido!");
+            }
+            catch (DivideByZeroException)
+            {
+                Console.WriteLine("Erro: Não é possível dividir por zero!");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Ocorreu um erro inesperado: {ex.Message}");
+            }
+            finally
+            {
+                Console.WriteLine("Operação finalizada.");
+            }
+        }
+    }
+}
