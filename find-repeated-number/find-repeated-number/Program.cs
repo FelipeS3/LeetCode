a00b7c5b40978a26d5e7d7db7a36eee7c7456847
FelipeS3 <Felipexy50@gmail.com>
2025-03-25 16:30:45 -0300

    Implement last occurrence search for a letter in a string

---
+++ find-repeated-number/find-repeated-number/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace find_repeated_number
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = {1,2,3,4,2,5,6};
+
+            int resultado = RepeatedNumber(array);
+
+            Console.WriteLine(resultado);
+        }
+
+        private static int RepeatedNumber(int[] array)
+        {
+            Dictionary<int, int> contagens = new Dictionary<int, int>();
+
+            foreach (int num in array)
+            {
+                if (contagens.ContainsKey(num))
+                {
+                    contagens[num]++;
+                }
+                else
+                {
+                    contagens[num] = 1;
+                }
+            }
+
+            foreach (var par in contagens)
+            {
+                if (par.Value > 1)
+                {
+                    return par.Key;
+                }
+            }
+
+            return -1;
+        }
+    }
+}
