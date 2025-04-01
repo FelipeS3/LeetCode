2a741804c9f236d447fed3e9120be57412bc5468
FelipeS3 <Felipexy50@gmail.com>
2025-04-01 12:03:50 -0300

    final-commit

--- BubbleSort/BubbleSort/Program.cs
+++ BubbleSort/BubbleSort/Program.cs
@@ -1,4 +1,5 @@
-﻿using System.Runtime.InteropServices.JavaScript;
+﻿using System.Reflection.PortableExecutable;
+using System.Runtime.InteropServices.JavaScript;
 
 namespace BubbleSort
 {
@@ -6,7 +7,7 @@ namespace BubbleSort
     {
         static void Main(string[] args)
         {
-            int[] test1 = { 4, 1, 3, 2 };
+            int[] test1 = { 4, 1, 3, 2,5,10,9,8,7,6,5,4,3,2,2,1 };
             int[] test2 = { 5, 2, 8 };
             int[] test3 = { 1 };
             int[] test4 = { 3, 3, 1, 5 };
@@ -15,8 +16,6 @@ namespace BubbleSort
             Console.WriteLine(string.Join(", ", BubbleSort(test2))); // 2, 5, 8
             Console.WriteLine(string.Join(", ", BubbleSort(test3))); // 1
             Console.WriteLine(string.Join(", ", BubbleSort(test4))); // 1, 3, 3, 5
-            
-
         }
 
         private static int[] BubbleSort(int[] numbers)
@@ -24,7 +23,7 @@ namespace BubbleSort
             var array = new int[numbers.Length];
             Array.Copy(numbers, array, numbers.Length);
 
-            for (int i = 0; i < array.Length - 1; i++)
+            for(int i = 0; i < numbers.Length - 1; i++)
             {
                 for (int j = 0; j < array.Length - 1 - i; j++)
                 {
@@ -37,5 +36,8 @@ namespace BubbleSort
 
             return array;
         }
+
+        
     }
 }
+   
