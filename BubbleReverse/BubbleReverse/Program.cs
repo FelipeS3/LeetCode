e44c59e94d7d32fca8656a1093c08dd81beb0434
FelipeS3 <Felipexy50@gmail.com>
2025-04-01 15:06:53 -0300

    initial-commit

---
+++ BubbleReverse/BubbleReverse/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace BubbleReverse
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = new[] { 1, 2, 3, 4, 5, 6 };
+            Console.WriteLine(string.Join(",", BubbleSortReverse(numbers)));
+        }
+
+        private static int[] BubbleSortReverse(int[] numbers)
+        {
+            int[] array = new int[numbers.Length];
+            Array.Copy(numbers, array, numbers.Length);
+
+            for (int i = 0; i < array.Length - 1; i++)
+            {
+                for (int j = 0; j < array.Length - 1 - i; j++)
+                {
+                    if (array[j] < array[j + 1])
+                    {
+                        (array[j + 1], array[j]) = (array[j], array[j + 1]);
+                    }
+                }
+            }
+
+            return array;
+        }
+    }
+}
