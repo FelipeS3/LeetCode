8c32996156be7875036ec4a408250ff8503be4cf
FelipeS3 <Felipexy50@gmail.com>
2025-04-01 15:10:58 -0300

    initial-commit

--- BubbleSort/BubbleSort/Program.cs
+++ BubbleSort/BubbleSort/Program.cs
@@ -1,21 +1,27 @@
-﻿using System.Reflection.PortableExecutable;
-using System.Runtime.InteropServices.JavaScript;
-
+﻿
 namespace BubbleSort
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            int[] test1 = { 4, 1, 3, 2,5,10,9,8,7,6,5,4,3,2,2,1 };
+            int[] test1 = { 4, 1, 3, 2, 5, 10, 9, 8, 7, 6, 5, 4, 3, 2 ,2 ,1 };
             int[] test2 = { 5, 2, 8 };
             int[] test3 = { 1 };
-            int[] test4 = { 3, 3, 1, 5 };
+            int[] test4 = { 3, 3, 1, 5, 7, 6 };
 
             Console.WriteLine(string.Join(", ", BubbleSort(test1))); // 1, 2, 3, 4
             Console.WriteLine(string.Join(", ", BubbleSort(test2))); // 2, 5, 8
             Console.WriteLine(string.Join(", ", BubbleSort(test3))); // 1
             Console.WriteLine(string.Join(", ", BubbleSort(test4))); // 1, 3, 3, 5
+            Console.WriteLine(string.Join(", ", BubbleSortInverse(test1)));
+            Console.WriteLine(string.Join(", ", SelectionSort(test4)));
+
+            int[] test7 = { 4, 1, 3, 2 };
+            Console.WriteLine(string.Join(", ", SelectionSort(test7))); // 1, 2, 3, 4
+
+            int[] testSort = [3, 1, 2, 4];
+            Console.WriteLine(string.Join(", ", SortArrayByParity(testSort)));
         }
 
         private static int[] BubbleSort(int[] numbers)
@@ -37,7 +43,64 @@ namespace BubbleSort
             return array;
         }
 
-        
+        private static int[] BubbleSortInverse(int[] numbers)
+        {
+            int[] array = new int[numbers.Length];
+            Array.Copy(numbers, array, numbers.Length);
+
+            for (int i = 0; i < array.Length - 1; i++) 
+            {
+                for (int j = 0; j < array.Length - 1 - i; j++)
+                {
+                    if (array[j] < array[j + 1])
+                    {
+                        (array[j + 1], array[j]) = (array[j], array[j + 1]);
+                    }
+                }
+            }
+
+            return array;
+        }
+
+        private static int[] SelectionSort(int[] numbers)
+        {
+            int[] array = new int[numbers.Length];
+            Array.Copy(numbers, array, numbers.Length);
+
+            for (int i = 0; i < array.Length - 1; i++)
+            {
+                var minIndex = i;
+                for (int j = 1 + i; j < array.Length; j++)
+                {
+                    if (array[j] < array[minIndex])
+                    {
+                        minIndex = j;
+                    }
+                }
+                (array[i], array[minIndex]) = (array[minIndex], array[i]);
+            }
+
+            return array;
+        }
+
+        public static int[] SortArrayByParity(int[] nums)
+        {
+            int[] array = new int[nums.Length];
+            Array.Copy(nums, array, nums.Length);
+
+            for (int j = 0; j < array.Length; j++)
+            {
+                for (int i = 0; i < array.Length - 1; i++)
+                {
+                    if (array[i] % 2 == 0)
+                    {
+                        array[i] = j;
+                    }
+                }
+            }
+
+            return array;
+        }
     }
 }
    
--- practice-challenges/practice-challenges/Program.cs
+++ practice-challenges/practice-challenges/Program.cs
@@ -4,79 +4,12 @@ public class Program
 {
     static void Main(string[] args)
     {
-        //Exercicio 1
-        string Nome = "Felipe";
-        int Idade = 20;
-        Console.WriteLine($"{Nome} e {Idade}");
+        int[] array1 = [1, 2, 3, 4];
+        int[] array2 = [1, 2, 3, 4];
+    }
 
-        //Exercicio 2
-        Console.WriteLine("Digite sua idade");
-        string idade = Console.ReadLine();
-
-        int idadeInteiro = int.Parse(idade);
-
-        Console.WriteLine($"Idade: {idadeInteiro + 5}");
-
-        //Exercicio 3
-        int CalcularDobro(int valor)
-        {
-            return valor * 2;
-        }
-
-        //Exercicio 4
-        Console.WriteLine("Digite um numero");
-        int numero = int.Parse(Console.ReadLine());
-        // for (int i = 1; i <= 10; i++)
-        // {
-        //     Console.WriteLine($"Tabuada do {numero} é {numero * i}");
-        // 
-        int tabuada = 0;
-        while (tabuada <= 10)
-        {
-            tabuada++;
-            Console.WriteLine($"A tabuada do {numero} é {numero * tabuada}");
-        }
-
-        int MaiorValor(int x, int y)
-        {
-            return x > y ? x : y;
-        }
-
-
-        List<int> valores = new List<int>();
-
-        Console.WriteLine("Digite primeiro valor");
-        int valor1 = int.Parse(Console.ReadLine());
-        valores.Add(valor1);
-
-        Console.WriteLine("Digite segundo valor");
-        int valor2 = int.Parse(Console.ReadLine());
-        valores.Add(valor2);
-
-        Console.WriteLine("Digite terceiro valor");
-        int valor3 = int.Parse(Console.ReadLine());
-        valores.Add(valor3);
-
-        Console.WriteLine("Digite quarto valor");
-        int valor4 = int.Parse(Console.ReadLine());
-        valores.Add(valor4);
-
-        Console.WriteLine("Digite quinto valor");
-        int valor5 = int.Parse(Console.ReadLine());
-        valores.Add(valor5);
-
-        int valorMaior = EncontrarMaior(valores);
-        Console.WriteLine(valorMaior);
-
-        int EncontrarMaior(List<int> valores)
-        {
-            int maior = valores[0];
-            foreach (var valor in valores)
-            {
-                if (valor > maior) maior = valor;
-            }
-            return maior;
-        }
+    private static int[] ReverseEvenNumbers(int[] numbers)
+    {
 
     }
 
